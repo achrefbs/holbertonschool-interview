cb7d8526c9476e495adcdad83ccc85cd5a55625c
achref <achrefbs>
2021-01-08 13:35:48 +0100

    fix

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     """
     minOperations min operations
@@ -14,6 +17,8 @@ def minOperations(n):
     i5 = 0
     i7 = 0
     i11 = 0
+    if (n <= 0):
+        return 0
     while (t % 2 == 0):
         i2 += 1
         t = t / 2
