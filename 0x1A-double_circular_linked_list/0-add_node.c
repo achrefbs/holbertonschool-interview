10512ff72ee233fe588049c6bae4b37bb779942a
achrefbs <siachrefboularess@gmail.com>
2021-09-24 14:46:54 +0100

    betty

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -7,31 +7,31 @@
  */
 List *add_node_begin(List **h, char *str)
 {
-    List *last, *n = NULL;
+	List *last, *n = NULL;
 
-    n = malloc(sizeof(List));
+	n = malloc(sizeof(List));
 
-    if (n == NULL)
-        return (NULL);
-    n->str = strdup(str);
-    if (!n->str)
-    {
-        free(n);
-        return (NULL);
-    }
-    if (*h == NULL)
-    {
-        n->next = n->prev = n;
-        *h = n;
-        return (n);
-    }
-    last = (*h)->prev;
-    n->next = (*h);
-    n->prev = last;
-    (*h)->prev = last->next = n;
-    *h = n;
+	if (n == NULL)
+		return (NULL);
+	n->str = strdup(str);
+	if (!n->str)
+	{
+		free(n);
+		return (NULL);
+	}
+	if (*h == NULL)
+	{
+		n->next = n->prev = n;
+		*h = n;
+		return (n);
+	}
+	last = (*h)->prev;
+	n->next = (*h);
+	n->prev = last;
+	(*h)->prev = last->next = n;
+	*h = n;
 
-    return (n);
+	return (n);
 }
 /**
  * add_node_end - add node at end
@@ -41,30 +41,30 @@ List *add_node_begin(List **h, char *str)
  */
 List *add_node_end(List **h, char *str)
 {
-    List *last, *n = NULL;
+	List *last, *n = NULL;
 
-    n = malloc(sizeof(List));
+	n = malloc(sizeof(List));
 
-    if (n == NULL || str == NULL)
-        return (NULL);
+	if (n == NULL || str == NULL)
+		return (NULL);
 
-    n->str = strdup(str);
-    if (!n->str)
-    {
-        free(n);
-        return (NULL);
-    }
-    if (*h == NULL)
-    {
-        n->next = n->prev = n;
-        *h = n;
-        return (n);
-    }
-    last = (*h)->prev;
-    last->next = n;
-    n->next = *h;
-    n->prev = last;
-    (*h)->prev = n;
+	n->str = strdup(str);
+	if (!n->str)
+	{
+		free(n);
+		return (NULL);
+	}
+	if (*h == NULL)
+	{
+		n->next = n->prev = n;
+		*h = n;
+		return (n);
+	}
+	last = (*h)->prev;
+	last->next = n;
+	n->next = *h;
+	n->prev = last;
+	(*h)->prev = n;
 
-    return (n);
+	return (n);
 }
